0f769bccd05186a0874cafedc1c1e229e519c2e6
Herdina <gherdina7@gmail.com>
2023-07-18 23:04:25 -0300

    feat: created a router with animatePresence and cloneElement

---
+++ src/Router.tsx
@@ -0,0 +1,29 @@
+import { cloneElement } from 'react';
+import { useRoutes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import About from './pages/About';
+
+function Router() {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/projects', element: <Projects /> },
+    { path: '/skills', element: <Skills /> },
+    { path: '/about', element: <About /> },
+  ]);
+
+  const { pathname } = useLocation();
+
+  if (!routes) return null;
+
+  return (
+    <AnimatePresence mode="wait">
+      {cloneElement(routes, { key: pathname })}
+    </AnimatePresence>
+  );
+}
+
+export default Router;
