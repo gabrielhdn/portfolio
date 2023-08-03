6537ea918f2b5d095e8c305c5c62361ca941206b
Herdina <gherdina7@gmail.com>
2023-08-03 04:08:03 -0300

    feat: strict mode temporarily disabled to prevent hover-effect bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -6,11 +5,11 @@ import ThemeProvider from './context/ThemeProvider';
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>/
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  // </React.StrictMode>,
 );
